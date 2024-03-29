6698cea64866407ec410c3808df635f656b2da2d
eff3ry <jeffery.tolmie@gmail.com>
2024-03-29 16:17:21 +1300

    App Class Creation

---
+++ COMP603_Proj1/src/VirtualPet/App.java
@@ -0,0 +1,16 @@
+package VirtualPet;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jeffery & Josh
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
