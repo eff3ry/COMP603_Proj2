790126d132ad4f254bce7cdb8d0fbdaf57f7ab73
eff3ry <jeffery.tolmie@gmail.com>
2024-03-29 17:50:20 +1300

    Update Pet.java

--- COMP603_Proj1/src/VirtualPet/Pet.java
+++ COMP603_Proj1/src/VirtualPet/Pet.java
@@ -4,6 +4,10 @@
  */
 package VirtualPet;
 
+import java.util.Random;
+
+
+
 /**
  *
  * @author jeffe
@@ -15,9 +19,10 @@ public class Pet {
     
     int health;
     
-    //Hunger Attributes 0-100
-    int water;
-    int food;
+    //Hunger Attributes 0-100, these are 'negative' attributes so the lower the better
+    //could display a low value as green, mid value as orange and high value as red
+    int thirst;
+    int hunger;
     
     int age;
     int weight;
@@ -32,13 +37,78 @@ public class Pet {
     int laziness; // how lazy/active
     int likesBeingPetted;
             
+    //Resources 0-100
+    int foodInBowl;
+    int waterInBowl;
    
+    static Random rand = new Random();   
     
-    
+    //Pet Creation
     public Pet(String name)
     {
         this.name = name;
+        initResources();
+        initNeeds();
     }
     
-   
+    private void initResources()
+    {
+        foodInBowl = 50;
+        waterInBowl = 50;
+    }
+    
+    private void initNeeds()
+    {
+        hunger = 50;
+        thirst = 50;
+    }
+    
+    
+    public void tickTime()
+    {
+        //Increase thirst
+        thirst += rand.nextInt(0, 10);
+        thirst = thirst > 100 ? 100 : thirst; //cap at 100
+        //Increase hunger
+        hunger += rand.nextInt(0, 10);
+        hunger = hunger > 100 ? 100 : hunger; //cap at 100
+    }
+    
+    private boolean canEat()
+    {
+        return hunger > 0 && foodInBowl > 0;
+    }
+    
+    private boolean canDrink()
+    {
+        return thirst > 0 && waterInBowl > 0;
+    }
+        
+    
+    private void eatAction()
+    {
+        int eatAmmount = Math.min(hunger, foodInBowl); // clamp eat ammount to needs/foodinbowl
+        eatAmmount = Math.min(rand.nextInt(10, 20), eatAmmount); // now apply random ammount clamped
+         
+        hunger -= eatAmmount;
+        foodInBowl -= eatAmmount; 
+    }
+    
+    
+    /**
+     * @param amount the amount to fill the bowl by
+     * @return amount past max value (100)
+     */
+    private int fillFoodBowl(int amount)
+    {
+        if (foodInBowl + amount > 100)
+        {   
+            int oldVal = foodInBowl;
+            foodInBowl = 100;
+            return oldVal + amount - 100;
+        } else {
+            foodInBowl += amount;
+            return 0;
+        }
+    }
 }
