5ba54b5c93a9c15f934715928d68e2670df8c702
eff3ry <jeffery.tolmie@gmail.com>
2024-03-29 16:17:11 +1300

    PetSetup

---
+++ COMP603_Proj1/src/VirtualPet/Pet.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VirtualPet;
+
+/**
+ *
+ * @author jeffe
+ */
+import Effects.*;
+
+public class Pet {
+    String name;
+    
+    int health;
+    int water;
+    int food;
+    
+    int age;
+    int weight;
+    int strength;
+    
+    int mood; //angry, sad, happy etc
+    int friendliness; //rename to sociability??
+    
+    int exercise;
+    
+    int pickiness; //how picky it is with food
+    int laziness; // how lazy/active
+    int likesBeingPetted;
+            
+    Effect[] effects;
+}
