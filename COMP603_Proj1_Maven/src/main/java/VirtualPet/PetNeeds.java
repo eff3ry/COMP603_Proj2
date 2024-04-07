aedb518260771aad862beb4d6392beb354ad1503
eff3ry <jeffery.tolmie@gmail.com>
2024-04-07 12:05:34 +1200

    Migrate to maven :angry: :bomb:

---
+++ COMP603_Proj1_Maven/src/main/java/VirtualPet/PetNeeds.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VirtualPet;
+
+/**
+ *
+ * @author jeffe
+ */
+public class PetNeeds {
+    
+    //Hunger Attributes 0-100, these are 'negative' attributes so the lower the better
+    //could display a low value as green, mid value as orange and high value as red
+    public int thirst;
+    public int hunger;
+    int sadness; //angry, sad, happy etc
+    public int exercise;
+    
+    public int bladder;
+    
+    public void initNeeds()
+    {
+        hunger = 50;
+        thirst = 50;
+        sadness = 50;
+    }
+}
---
+++ COMP603_Proj1_Maven/src/main/java/VirtualPet/PetResources.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VirtualPet;
+
+/**
+ *
+ * @author jeffe
+ */
+public class PetResources {
+    
+    //Resources 0-100
+    public int foodInBowl;
+    public int waterInBowl;
+    
+    public void initResources()
+    {
+        foodInBowl = 50;
+        waterInBowl = 50;
+    }
+}
