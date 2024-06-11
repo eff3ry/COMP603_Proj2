3be1cc83dfa2395f0952b736889fd16ba1166069
eff3ry <jeffery.tolmie@gmail.com>
2024-06-11 14:36:41 +1200

    test db table creation

--- COMP603_Proj2_Maven/src/main/java/VirtualPet/Database/PetManager.java
+++ COMP603_Proj2_Maven/src/main/java/VirtualPet/Database/PetManager.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author jeffe
@@ -15,6 +17,15 @@ import java.sql.Statement;
 public class PetManager {
     
     DBManager dbManager;
+    
+    public static void main(String[] args) {
+        PetManager pm = new PetManager();
+        try {
+            pm.createPetTable();
+        } catch (SQLException ex) {
+            Logger.getLogger(PetManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
     
     public PetManager()
     {
@@ -29,7 +40,16 @@ public class PetManager {
         {
             //create table
             Statement s = conn.createStatement();
-            s.execute("CREATE TABLE PET (name VARCHAR(255) NOT NULL PRIMARY KEY, species VARCHAR(255) NOT NULL, sadness INTEGER NOT NULL DEFAULT 0, thirst INTEGER NOT NULL DEFAULT 50, hunger INTEGER NOT NULL DEFAULT 50, bladder INTEGER NOT NULL DEFAULT 50, exercise INTEGER NOT NULL DEFAULT 50, food INTEGER NOT NULL DEFAULT 0, water INTEGER NOT NULL DEFAULT 0)");
+            s.execute("CREATE TABLE PET ("
+                    + "name VARCHAR(255) NOT NULL PRIMARY KEY,"
+                    + "species VARCHAR(255) NOT NULL,"
+                    + "sadness INTEGER NOT NULL DEFAULT 0,"
+                    + "thirst INTEGER NOT NULL DEFAULT 50,"
+                    + "hunger INTEGER NOT NULL DEFAULT 50,"
+                    + "bladder INTEGER NOT NULL DEFAULT 50,"
+                    + "exercise INTEGER NOT NULL DEFAULT 50,"
+                    + "food INTEGER NOT NULL DEFAULT 0,"
+                    + "water INTEGER NOT NULL DEFAULT 0)");
             
         }
     }
